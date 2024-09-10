2728ac921e405a3c82b39c65e74af62213d6963a
Stina Persson <stpe1901@student.miun.se>
2024-09-10 17:13:39 +0200

    Lagt in beskrivning på bubble sort

--- Program.cs
+++ Program.cs
@@ -9,5 +9,15 @@ I denna uppgift ska algorytmen bubble sort jämföras med C# inbygda sortering.
 Detta ska testas på en array med 100 värden i 
 */
 
+/*
+Bubble sort innebär att värdena i ex en array jämförs med varandra genom att 
+det värde som är bredvid. 
+Om tex 9 är bredvid 3 i en array med stigande ordning så byter 9 och 3 plats. 
+Man behöver alltså jämföra värdena med varandra i den ordningen som de står för att
+till sist få en ordnad lista efter x antal loopar. 
+*/
+
+//Bubble sort metod 
+
 Console.WriteLine("test"); 
 
