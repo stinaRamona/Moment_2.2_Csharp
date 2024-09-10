86dcde37f1cfac996e209dc09f0eebd82d667408
Stina Persson <stpe1901@student.miun.se>
2024-09-10 16:28:33 +0200

    Lägger in filer i repo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Laboration 2.2 i kursen Programmering i C#.NET, HT24, Mittuniversitetet 
+Kod skriven av Stina Persson (stpe1901)
+Senast uppdaterad 10/9 - 24 
+*/
+
+/*
+I denna uppgift ska algorytmen bubble sort jämföras med C# inbygda sortering. 
+Detta ska testas på en array med 100 värden i 
+*/
+
+Console.WriteLine("test"); 
+
